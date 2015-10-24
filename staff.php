bd49095605ab1227d0c7f2f3839649357d38537d
PsyKooGlitch <psykogltich@gmail.com>
2015-10-24 02:33:01 +0200

    Le site

---
+++ staff.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>GHOSTHRONE</title>
+
+<link href="http://fonts.googleapis.com/css?family=Oswald:400,700,300" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" href="stylesheets/main.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+<link rel="stylesheet" href="stylesheets/post.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+<link rel="stylesheet" href="stylesheets/validationEngine.jquery.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+<link rel="stylesheet" href="stylesheets/jquery.fancybox1c51.css?v=2.1.2" type="text/css"  media="screen" />
+
+</head>
+
+<body>
+
+<div id="footer_image">
+<div id="main_wrapper">
+
+
+
+    <div id="main_in">
+    
+
+    <div id="menu_wrapper">
+      <div id="menu_left"></div>
+      <ul id="menu">
+        <li><a href="index.php">Home</a></li>
+        <li><a href=javascript:Ts()>TeamSpeak</a></li>
+        <li><a href="rules.php">Le réglements</a></li>
+        <li><a href="#">Le Forum</a></li>
+        <li><a href="staff.php">Le Staff</a></li>
+        <li><a href="#">Nous Contacter</a></li>
+      </ul>
+      <div id="menu_right"></div>
+    </div>
+    
+
+	
+    <table cellpadding="0" cellspacing="0" width="958px" id="main_news_wrapper">
+      <tr> 
+        
+
+        <td valign="top" id="left_wrapper"><div class="header">
+            <h2><span>GHOSTHRONE //</span> GENERAL NEWS</h2>
+          </div>
+          <ul id="general_news">
+            <li>
+              <div class="image"><a href="#"><img alt="alt_example" src="images/staff/psykoo.png" /></a></div>
+              <ul class="social_share">
+                <li><a href="#"><img alt="alt_example" src="images/fbk.png" border="0" /></a></li>
+                <li><a href="#"><img alt="alt_example" src="images/twitter.png" border="0" /></a></li>
+                <li><a href="#"><img alt="alt_example" src="images/more.png" border="0" /></a></li>
+              </ul>
+              <div class="info">
+                
+                <h2><a href="#">PsyKooGlitch</h2>
+                <div class="date_n_author">Webmaster</div>
+                <p>ces qualités</p>
+                <a href="#" class="read_more2">En savoir plus</a> </div>
+            </li>
+            <li>
+              <div class="image"><a href="#"><img alt="alt_example" src="images/staff/energiik.png" /></a></div>
+              <ul class="social_share">
+                <li><a href="#"><img alt="alt_example" src="images/fbk.png" border="0" /></a></li>
+                <li><a href="#"><img alt="alt_example" src="images/twitter.png" border="0" /></a></li>
+                <li><a href="#"><img alt="alt_example" src="images/more.png" border="0" /></a></li>
+              </ul>
+             <div class="info">
+                
+                <h2><a href="#">EnerGiik</h2>
+                <div class="date_n_author">Player</div>
+                <p>Ces qualités</p>
+                <a href="#" class="read_more2">En savoir plus</a> </div>
+            </li>
+          </ul>
+
+		  
+        <td valign="top" id="right_wrapper">
+
+          <div id="top_games">
+            <div class="header"><a href="#">Classment de joueurs</a></div>
+            <ul>
+              <li>
+                <div class="img"><img alt="alt_example" src="images/staff/psykoo.png" /></a></div>
+                <div class="info"> PsyKooGlitch</a><br/>
+                  <small>Depuis, 28/09/15</small><br/>
+                  <img alt="alt_example" src="images/stars.png" /> </div>
+              </li>
+              
+            </ul>
+            <div class="footer"></div>
+          </div>
+          
+          <div id="categories">
+            <div class="header"><a href="#">Categories</a></div>
+            <ul>
+              <li> <a href="#">Action</a> </li>
+              <li> <a href="#">Aventure</a> </li>
+              <li> <a href="#">Stratégie</a> </li>
+              <li> <a href="#">RPG</a> </li>   
+            </ul>
+            <div class="footer"></div>
+          </div>
+      
+
+      
+        </td>
+      
+        </tr>
+      
+    </table>
+    <div class="bottom_shadow"></div>
+
+
+		                  
+        
+
+
+
+    <div id="footer">
+      <div id="about">
+        <div class="header"><a href="#">A propos de Nous</a></div>
+        <div class="body">
+          <p>Blablalalalalal</p>
+          <p>bla</p>
+          </div>
+        <div class="footer"></div>
+      </div>
+      <div class="divider_footer"></div>
+      <div id="latest_media">
+        <div class="header"><a href="#">Nos actus</a></div>
+        <div class="body">
+          <ul id="l_media_list">
+            <li>actu</a></li>
+            <li>actu</a></li>
+            <li>actu</a></li>
+            <li>actu</a></li>
+            <li>actu</a></li>
+            <li>actu</a></li>
+            <li>actu</a></li>
+            <li>actu</a></li>
+            <li>actu</a></li>
+          </ul>
+        </div>
+        <div class="footer"></div>
+      </div>
+      <div class="clear"></div>
+    </div>
+
+	
+    <div id="twitter_last"> <a id="tr_left" href="#"><img alt="alt_example" src="images/blank.gif" width="100%" height="30px" border="0" /></a>
+      <div id="tr_right">
+        <ul id="tw">
+        </ul>
+      </div>
+    </div>
+
+    
+  </div>
+
+  
+  <a id="cop_text" href="#"> Made by PsyKooGlitch</a> 
+  </div>
+</div>
+
+
+<script src="../../../code.jquery.com/jquery-latest.js" type="text/javascript"></script> 
+<script src="javascript/jquery.carouFredSel-6.1.0.js" type="text/javascript"></script> 
+<script src="javascript/jquery.cslider.js" type="text/javascript" ></script> 
+<script src="javascript/modernizr.custom.28468.js" type="text/javascript"></script> 
+<script src="javascript/getTweet.js" type="text/javascript" ></script> 
+<script src="javascript/jquery.fancybox62ba.js?v=2.1.3" type="text/javascript" ></script> 
+
+<script>
+function Ts() {
+    var pseudo = prompt("Entrez votre pseudo");
+    if (pseudo != null) {
+        window.location = "ts3server://ts3.juicylife.austri-online.com/?nickname=" + pseudo
+    }
+}
+</script>
+</body>
+</html>
